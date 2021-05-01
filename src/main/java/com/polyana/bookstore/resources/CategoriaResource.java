4d27eac9b00dc63059f746e4777e170b5f19908d
Polyana <polyanacsousa@gmail.com>
2021-05-01 20:48:34 -0300

    Implementação do método POST para categoria

--- src/main/java/com/polyana/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/polyana/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,6 @@
 package com.polyana.bookstore.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.polyana.bookstore.domain.Categoria;
 import com.polyana.bookstore.dtos.CategoriaDTO;
@@ -35,6 +38,17 @@ public class CategoriaResource {
 		List<Categoria> list = categoriaService.findAll();
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+		obj = categoriaService.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		//Retorna a uri de acesso
+		return ResponseEntity.created(uri).build();
+		//Retorna o objeto
+		//return ResponseEntity.created(uri).body(obj);
 		
 	}
 	
