5116f30eece526c899ff86d59af08ead8ca7680a
Polyana <polyanacsousa@gmail.com>
2021-05-04 16:56:44 -0300

    Busca por livros por categoria

--- src/main/java/com/polyana/bookstore/resources/LivroResource.java
+++ src/main/java/com/polyana/bookstore/resources/LivroResource.java
@@ -1,14 +1,20 @@
 package com.polyana.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.polyana.bookstore.domain.Livro;
 import com.polyana.bookstore.service.LivroService;
+
+import com.polyana.bookstore.dtos.LivroDTO;
 
 @RestController
 @RequestMapping(value="/livros")
@@ -21,6 +27,16 @@ public class LivroResource {
 	public ResponseEntity<Livro> findById(@PathVariable Integer id){
 		Livro obj = livroService.findById(id);
 		return ResponseEntity.ok().body(obj);
+		
+	}
+	
+	//Aqui temos que receber como parametro o ID da categoria
+	//localhost:8080/livros?categoria=1
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value="categoria", defaultValue = "0") Integer id_cat){
+		List<Livro> list = livroService.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 		
 	}
 
--- src/main/java/com/polyana/bookstore/service/LivroService.java
+++ src/main/java/com/polyana/bookstore/service/LivroService.java
@@ -1,5 +1,6 @@
 package com.polyana.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +16,22 @@ public class LivroService {
 	@Autowired
 	private LivroRepository livroRepository;
 	
+	@Autowired
+	private CategoriaService categoriaService;
+	
 	public Livro findById(Integer id) {
 		Optional<Livro> obj = livroRepository.findById(id);
 		return obj.orElseThrow(() -> new  ObjectNotFoundException(
 				"Livro não  encontrado " + id + " Tipo: " + Livro.class.getName()));
+	}
+
+	public List<Livro> findAll(Integer id_cat) {
+		
+		//Primeiro verificamos se o id da categoria realmente existe
+		categoriaService.findById(id_cat);
+		
+		//Este metodo não existe então foi criado a mão
+		return livroRepository.findAllByCategoria(id_cat);
 	}
 	
 
