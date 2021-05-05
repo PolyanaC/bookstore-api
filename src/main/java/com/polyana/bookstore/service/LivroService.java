538fc709576eb886055871cf14cded74ca8fd7a7
Polyana <polyanacsousa@gmail.com>
2021-05-04 21:01:12 -0300

    Método POST de livro

--- src/main/java/com/polyana/bookstore/service/LivroService.java
+++ src/main/java/com/polyana/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.polyana.bookstore.domain.Categoria;
 import com.polyana.bookstore.domain.Livro;
 import com.polyana.bookstore.repositories.LivroRepository;
 import com.polyana.bookstore.service.exceptions.ObjectNotFoundException;
@@ -44,6 +45,13 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return livroRepository.save(obj);
 	}
 	
 
